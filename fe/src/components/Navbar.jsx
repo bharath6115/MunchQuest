29556356b27073b8c62da7353a5028b15c796230
bharath6115 <153836615+bharath6115@users.noreply.github.com>
2025-07-04 15:25:49 +0530

    made navbar mobile responsive

--- fe/src/components/Navbar.jsx
+++ fe/src/components/Navbar.jsx
@@ -4,38 +4,64 @@ import { signOut } from "firebase/auth";
 import { Link } from "react-router";
 import { useAuth } from "../services/firebaseMethods";
 import logo from "../../public/logo.png"
+import { IoMenuOutline } from "react-icons/io5";
+import { useState } from "react";
+
+const linkStyles = ({ isActive = false }) => {
+    return [
+        isActive ? "text-bold" : "",
+        isActive ? "text-yellow-300" : "text-zinc-300",
+        isActive ? "" : "hover:text-white",
+        isActive ? "border-double border-yellow-300 border-y-2" : "",
+        "py-2"
+    ].join(" ")
+}
+
+const NavLinks = () => {
+
+    return (
+        <>
+            <NavLink to="/" className={linkStyles} end> Home </NavLink>
+            <NavLink to="/restaurants" className={linkStyles} end> Restaurants</NavLink>
+            <NavLink to="/restaurants/new" className={linkStyles} end> Add Restaurant</NavLink>
+        </>
+    )
+}
 
 export default function Navbar() {
 
     const { isLoggedIn } = useAuth();
-
-    const navStyle = "bg-zinc-900 text-white border-b border-zinc-800 px-2 md:px-7 flex items-center justify-between shadow-md";
-    const linkStyles = ({ isActive = false }) => {
-        return [
-            isActive ? "text-bold" : "",
-            isActive ? "text-yellow-300" : "text-zinc-300",
-            isActive ? "" : "hover:text-white",
-            isActive ? "border-double border-yellow-300 border-y-2" : "",
-            "py-2"
-        ].join(" ")
-    }
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <nav className={navStyle}>
-            <div className="flex items-center p-1 flex-grow">
-                <img className="h-[50px] w-[49px]" src={logo} alt="logo" />
-                <NavLink to="/" className="hidden md:block text-white font-bold text-[20px]"> MunchQuest </NavLink>
+        <nav className="bg-zinc-900 text-white border-b border-zinc-800 px-2 md:px-7 flex items-center justify-between shadow-md">
+            {/* mobile nav */}
+            <div className="block md:hidden flex flex-grow items-center justify-start pl-3 text-4xl">
+                <IoMenuOutline className={`${menuOpen && "text-yellow-300"}`} onClick={() => { setMenuOpen(old => !old) }} />
+                {menuOpen &&
+                    <>
+                        <div className={`fixed top-[60px] left-0 h-full w-screen bg-zinc-800 p-4 transform ${menuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-100 z-50`}>
+                            <div onClick={()=>{setMenuOpen(false)}} className="mt-[200px] my-4 flex flex-col items-center justify-center text-[18px] gap-2">
+                                <NavLinks />
+                            </div>
+                        </div>
+                    </>
+                }
             </div>
-            <div className="flex gap-4 items-center justify-between px-4">
-                <NavLink to="/" className={linkStyles} end> Home </NavLink>
-                <NavLink to="/restaurants" className={linkStyles} end> Restaurants</NavLink>
-                <NavLink to="/restaurants/new" className={linkStyles} end> Add Restaurant</NavLink>
+            {/* Logo */}
+            <div className="flex items-center p-1 flex-grow justify-center md:justify-start">
+                <img className="h-[50px] w-[49px]" src={logo} alt="Website logo" />
+                <NavLink to="/" className="text-white font-bold text-[20px]"> MunchQuest </NavLink>
             </div>
+            {/* nav */}
+            <div className=" hidden md:flex gap-4 items-center justify-between px-4">
+                <NavLinks />
+            </div>
+            {/* login */}
             <div className="flex items-center gap-4 justify-end flex-grow">
-                <button> (~) </button>
                 {
                     isLoggedIn ?
-                        <Link to="/" onClick={async () => { await signOut(auth) }} className="text-bold hover:text-yellow-300 text-white"> Sign out </Link>
+                        <Link to="/" onClick={async () => { await signOut(auth) }} className="text-bold hover:text-yellow-300 text-white "> Sign out </Link>
                         :
                         <NavLink to="/login" className={linkStyles} end> Login </NavLink>
                 }
